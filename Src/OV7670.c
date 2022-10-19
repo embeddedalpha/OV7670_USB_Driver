556c45f63874201e4bef05b51fb65fa8b4a85464
Kunal Salvi <kunalsalvi63@gmail.com>
2022-10-18 21:53:51 -0500

    Repo update

--- Src/OV7670.c
+++ Src/OV7670.c
@@ -15,5 +15,23 @@ void OV7670_Init(I2C_TypeDef *port)
 	OV7670.mode = I2C_Fast_Mode;
 	I2C_Master_Init(OV7670);
 
-	I2C_Master_Write_Register(OV7670, OV7670_address, 0x00, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_GAIN, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_BLUE, 0x80);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_RED, 0x80);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM1, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_BAVE, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_GbAVE, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_AECHH, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_RAVE, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM2, 0x02);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM3, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM4, 0x00);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM5, 0x01);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM6, 0x43);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_AECH, 0x40);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_CLKRC, 0x87);
+	I2C_Master_Write_Register(OV7670, OV7670_address, OV7670_COM7, 0x04);
+
+
+
 }
